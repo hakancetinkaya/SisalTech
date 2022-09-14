c5149e9c56d9f7f0007741c8bf96de362f1e8308
hakancetinkaya <hakanckaya02@gmail.com>
2022-09-14 18:33:36 +0300

    Update EmployeeContext.js
    delete comment line

--- src/context/EmployeeContext.js
+++ src/context/EmployeeContext.js
@@ -5,57 +5,6 @@ import axios from "axios";
 export const EmployeeContext = createContext();
 
 const EmployeeContextProvider = (props) => {
-  const [employees, setEmployees] = useState([
-    {
-      id: uuidv4(),
-      name: "Thomas Hardy",
-      address: "89 Chiaroscuro Rd, Portland, USA",
-      openingHour: "12:30",
-      closingHour: "13:30",
-    },
-    {
-      id: uuidv4(),
-      name: "Dominique Perrier",
-      address: "Obere Str. 57, Berlin, Germany",
-      openingHour: "14:30",
-      closingHour: "15:30",
-    },
-    {
-      id: uuidv4(),
-      name: "Maria Anders",
-      address: "25, rue Lauriston, Paris, France",
-      openingHour: "10:30",
-      closingHour: "13:10",
-    },
-    {
-      id: uuidv4(),
-      name: "Fran Wilson",
-      address: "C/ Araquil, 67, Madrid, Spain",
-      openingHour: "20:30",
-      closingHour: "23:00",
-    },
-    {
-      id: uuidv4(),
-      name: "Martin Blank",
-      address: "Via Monte Bianco 34, Turin, Italy",
-      openingHour: "16:30",
-      closingHour: "22:40",
-    },
-    {
-      id: uuidv4(),
-      name: "Hakan Cetinkaya",
-      address: "89 Chiaroscuro Rd, İstanbul, Turkey",
-      openingHour: "12:30",
-      closingHour: "13:30",
-    },
-    {
-      id: uuidv4(),
-      name: "John Perrier",
-      address: "Obere Str. 57, Moscow, Russia",
-      openingHour: "14:30",
-      closingHour: "15:30",
-    },
-  ]);
 
   const [posts, setPosts] = useState([]);
 
